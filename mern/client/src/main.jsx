7796a8650ea41abd8d4598978469e598bc58bf53
VictorFromTheInternet <vma0430@gmail.com>
2025-03-09 22:18:08 -0500

    setting up routes on the main jsx file

--- mern/client/src/main.jsx
+++ mern/client/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 import App from './App.jsx'
+import Record from './components/RecordList'
+import RecordList from './components/RecordList'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <RecordList />
+      }
+    ]
+  }
+]  
 )
